ed636afd02bbab2cb8beaa7a1e30a2372da3c92c
sandeep <sandeep.cr007@gmail.com>
2019-09-27 16:03:55 -0400

    added interface div with working character shapes

--- p5/gameoflife.js
+++ p5/gameoflife.js
@@ -4,17 +4,30 @@ let rows;
 let board;
 let next;
 var xebraState;
+let init_shape_x, init_shape_y;
+
+//var xebraState;
+function preload(){
+  faces = loadImage('https://static.wixstatic.com/media/ca28c0_482d94bf038c4625bdfa5e21a7e40b86~mv2.png/v1/fill/w_26,h_26/happyface.png');
+  sadfaces = loadImage("https://static.wixstatic.com/media/ca28c0_97ee922c85e141179b65d6dea6dea3bf~mv2.png/v1/fill/w_26,h_26/dead4.png");
+
+}
 
 function setup() {
-  createCanvas( 500, 500);
-  w = 20;
+  emptiness = 135;
+  paused = false;
+
+  var canvas = createCanvas(500, 500);
+  canvas.parent('grid');
+  w = 22;
   connectXebra();
   // Adjust frameRate to change speed of generation/tempo of music
-  frameRate(0.2 );
+  frameRate(2);
 
   // Calculate columns and rows
   columns = floor(width / w);
   rows = floor(height / w);
+  console.log('Grid Size:',rows,columns);
 
   // Wacky way to make a 2D array is JS
   board = new Array(columns);
@@ -27,34 +40,27 @@ function setup() {
   for (i = 0; i < columns; i++) {
     next[i] = new Array(rows);
   }
-  init();
 }
 
+
 function draw() {
-  background(255);
-  generate();
-  
+  background(emptiness);
   for ( let i = 0; i < columns;i++) {
     for ( let j = 0; j < rows;j++) {
       if ((board[i][j] == 1)) {
-        fill(46,230,237);
+        image(faces,(i * w), (j * w),w);
+        //fill(46,230,237);
       }
-      else fill(0);
-      stroke(239,250,107,120);
-      rect(i * w, j * w, w-1, w-1);
-      sendToMax(board);
-    }
+      else {
+        image(sadfaces,(i * w), (j * w),w);
+      //stroke(emptiness);
+      //image(faces,(i * w)+w/2, (j * w)+w/2,w);
+      //fill(0);
+      //ellipse((i * w)+w/2, (j * w)+w/2, w, w);
+      }
+   }
   }
-
-}
-
-function sendToMax(val) {
-  
-}
-
-// reset board when mouse is pressed
-function mousePressed() {
-  init();
+  if (!paused) {console.log(paused); generate();}
 }
 
 // Fill board randomly
@@ -70,12 +76,125 @@ function init() {
   }
 }
 
+function initblock() {
+  console.log('block init');
+  console.log(init_shape_x, init_shape_y)
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      board[i][j] = 0;
+      next[i][j] = 0;
+    }
+  }
+  board[init_shape_x][init_shape_y] = 1;
+  board[init_shape_x+1][init_shape_y+1] = 1;
+  board[init_shape_x][init_shape_y+1] = 1;
+  board[init_shape_x+1][init_shape_y] = 1;
+  paused=false;
+}
+
+
+function initblinker() {
+  console.log('blinker init');
+  console.log(init_shape_x, init_shape_y)
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      board[i][j] = 0;
+      next[i][j] = 0;
+    }
+  }
+  board[init_shape_x][init_shape_y] = 1;
+  board[init_shape_x-1][init_shape_y] = 1;
+  board[init_shape_x+1][init_shape_y] = 1;
+  paused=false;
+}
+
+function inittoad() {
+  console.log('toad init');
+  console.log(init_shape_x, init_shape_y)
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      board[i][j] = 0;
+      next[i][j] = 0;
+    }
+  }
+  board[init_shape_x][init_shape_y] = 1;
+  board[init_shape_x+1][init_shape_y] = 1;
+  board[init_shape_x+2][init_shape_y] = 1;
+
+  board[init_shape_x-1][init_shape_y+1] = 1;
+  board[init_shape_x][init_shape_y+1] = 1;
+  board[init_shape_x+1][init_shape_y+1] = 1;
+  paused=false;
+}
+
+function initbeacon() {
+  console.log('beacon init');
+  console.log(init_shape_x, init_shape_y)
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      board[i][j] = 0;
+      next[i][j] = 0;
+    }
+  }
+  board[init_shape_x][init_shape_y] = 1;
+  board[init_shape_x+1][init_shape_y+1] = 1;
+  board[init_shape_x][init_shape_y+1] = 1;
+  board[init_shape_x+1][init_shape_y] = 1; 
+
+  board[init_shape_x-1][init_shape_y-1] = 1; 
+  board[init_shape_x-2][init_shape_y-1] = 1;
+  board[init_shape_x-2][init_shape_y-2] = 1;
+  board[init_shape_x-1][init_shape_y-2] = 1;
+  paused=false;
+  
+}
+
+function initglider() {
+  console.log('glider init');
+  console.log(init_shape_x, init_shape_y)
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      board[i][j] = 0;
+      next[i][j] = 0;
+    }
+  }
+  board[init_shape_x-1][init_shape_y+1] = 1; 
+  board[init_shape_x][init_shape_y+1] = 1;
+  board[init_shape_x+1][init_shape_y+1] = 1;
+  board[init_shape_x+1][init_shape_y] = 1;
+  board[init_shape_x][init_shape_y-1] = 1;
+  paused=false;
+}
+
+function initspaceship() {
+  console.log('spaceship init');
+  console.log(init_shape_x, init_shape_y);
+
+  board[init_shape_x][init_shape_y-1] = 1; 
+  board[init_shape_x+1][init_shape_y-1] = 1; 
+
+  board[init_shape_x-1][init_shape_y] = 1; 
+  board[init_shape_x-2][init_shape_y] = 1; 
+  board[init_shape_x+1][init_shape_y] = 1;
+  board[init_shape_x+2][init_shape_y] = 1;
+
+  board[init_shape_x-1][init_shape_y+1] = 1; 
+  board[init_shape_x-2][init_shape_y+1] = 1; 
+  board[init_shape_x][init_shape_y+1] = 1;
+  board[init_shape_x+1][init_shape_y+1] = 1;
+
+  board[init_shape_x][init_shape_y+2] = 1;
+  board[init_shape_x-1][init_shape_y+2] = 1;
+
+  paused=false;
+}
+
 // The process of creating the new generation
 function generate() {
+  console.log('Generating')
   // Loop through every spot in our 2D array and check spots neighbors
-  for (let x = 1; x < columns - 1; x++) {
-    for (let y = 1; y < rows - 1; y++) {
-      let newborns = [];
+  for (let x = 1; x < columns -1; x++) {
+    for (let y = 1; y < rows -1; y++) {
       // Add up all the states in a 3x3 surrounding grid
       let neighbors = 0;
       for (let i = -1; i <= 1; i++) {
@@ -88,13 +207,17 @@ function generate() {
       // we added it in the above loop
       neighbors -= board[x][y];
       // Rules of Life
-      if      ((board[x][y] == 1) && (neighbors <  2)) next[x][y] = 0;            // Loneliness
-      else if ((board[x][y] == 1) && (neighbors >  3)) next[x][y] = 0;            // Overpopulation
+      if ((board[x][y] == 1) && (neighbors <  2)) {                           // Loneliness
+        next[x][y] = 0; 
+      }            
+      else if ((board[x][y] == 1) && (neighbors >  3)) {                          // Overpopulation
+        next[x][y] = 0;
+      }        
       else if ((board[x][y] == 0) && (neighbors == 3)) {                          // Reproduction 
         next[x][y] = 1;
         xebraState.sendMessageToChannel("fromp5_born", ['hi',x,y]);
       } 
-      else next[x][y] = board[x][y];  // Stasis
+      else next[x][y] = board[x][y];                                              // Stasis
 
     }
   }
@@ -102,6 +225,40 @@ function generate() {
   let temp = board;
   board = next;
   next = temp;
+ }
+
+
+function mousePressed() {
+  paused=true;
+  let i = round((mouseX-(w/2))/w);
+  let j = round((mouseY-(w/2))/w);
+  
+
+  if ( i > rows -4 || j > columns -3 || i < 3 || j <3) {
+    console.log('skipping corner shapes',i,j);
+  }
+  else
+  {
+    init_shape_x = i;
+    init_shape_y = j;
+  }
+  // if (board[i][j] == 0){
+  //   board[i][j] = 1;
+  // } else{
+  //    board[i][j]= 0;
+  //   }
+}
+
+function keyPressed(){
+  if (keyCode === DOWN_ARROW){
+    paused=false;
+    initglider();
+  }
+  else if (keyCode === ENTER) {
+  paused = false;
+  // init();
+  }
+
 }
 
 function connectXebra() {
--- p5/p5_max.maxpat
+++ p5/p5_max.maxpat
@@ -10,7 +10,7 @@
 		}
 ,
 		"classnamespace" : "box",
-		"rect" : [ 34.0, 103.0, 1074.0, 708.0 ],
+		"rect" : [ 34.0, 103.0, 440.0, 709.0 ],
 		"bglocked" : 0,
 		"openinpresentation" : 0,
 		"default_fontsize" : 12.0,
@@ -39,12 +39,93 @@
 		"subpatcher_template" : "",
 		"boxes" : [ 			{
 				"box" : 				{
-					"id" : "obj-7",
-					"maxclass" : "newobj",
+					"id" : "obj-5",
+					"maxclass" : "slider",
 					"numinlets" : 1,
-					"numoutlets" : 0,
-					"patching_rect" : [ 731.0, 771.0, 41.0, 22.0 ],
-					"text" : "print y"
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"parameter_enable" : 1,
+					"patching_rect" : [ 293.0, 48.0, 20.0, 140.0 ],
+					"saved_attribute_attributes" : 					{
+						"valueof" : 						{
+							"parameter_longname" : "slider",
+							"parameter_shortname" : "slider",
+							"parameter_type" : 0
+						}
+
+					}
+,
+					"varname" : "slider"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-2",
+					"maxclass" : "plot~",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 210.0, 647.0, 50.0, 50.0 ],
+					"subplots" : [ 						{
+							"color" : [ 0.4, 0.4, 0.75, 1.0 ],
+							"thickness" : 3.0,
+							"point_style" : "circle",
+							"line_style" : "origin",
+							"number_style" : "none",
+							"filter" : "none",
+							"domain_start" : 0.0,
+							"domain_end" : 1.0,
+							"domain_style" : "linear",
+							"domain_markers" : [  ],
+							"domain_labels" : [  ],
+							"range_start" : 0.0,
+							"range_end" : 1.0,
+							"range_style" : "linear",
+							"range_markers" : [  ],
+							"range_labels" : [  ],
+							"origin_x" : 0.0,
+							"origin_y" : 0.0
+						}
+ ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-11",
+					"maxclass" : "newobj",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 73.333333333333314, 103.0, 38.666666666666686, 22.0 ],
+					"text" : "pack"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"columns" : 10,
+					"id" : "obj-10",
+					"maxclass" : "matrixctrl",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "list", "list" ],
+					"parameter_enable" : 1,
+					"patching_rect" : [ 73.333333333333314, 218.0, 356.0, 342.0 ],
+					"rows" : 10,
+					"saved_attribute_attributes" : 					{
+						"valueof" : 						{
+							"parameter_longname" : "matrixctrl",
+							"parameter_invisible" : 1,
+							"parameter_initial" : [ 6, 0, 1 ],
+							"parameter_shortname" : "matrixctrl",
+							"parameter_type" : 3
+						}
+
+					}
+,
+					"varname" : "matrixctrl"
 				}
 
 			}
@@ -53,9 +134,10 @@
 					"id" : "obj-6",
 					"maxclass" : "newobj",
 					"numinlets" : 1,
-					"numoutlets" : 0,
-					"patching_rect" : [ 668.0, 771.0, 41.0, 22.0 ],
-					"text" : "print x"
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 73.333333333333314, 132.0, 59.0, 22.0 ],
+					"text" : "append 1"
 				}
 
 			}
@@ -64,10 +146,10 @@
 					"id" : "obj-4",
 					"maxclass" : "newobj",
 					"numinlets" : 1,
-					"numoutlets" : 3,
-					"outlettype" : [ "", "int", "int" ],
-					"patching_rect" : [ 677.0, 716.0, 69.0, 22.0 ],
-					"text" : "unpack s i i"
+					"numoutlets" : 4,
+					"outlettype" : [ "", "", "int", "int" ],
+					"patching_rect" : [ 34.0, 48.0, 78.0, 22.0 ],
+					"text" : "unpack s s i i"
 				}
 
 			}
@@ -78,61 +160,32 @@
 					"numinlets" : 1,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 677.0, 684.000019068829261, 149.0, 22.0 ],
+					"patching_rect" : [ 34.0, 16.000019068829261, 149.0, 22.0 ],
 					"text" : "mira.channel fromp5_born"
 				}
 
 			}
 , 			{
 				"box" : 				{
-					"id" : "obj-1",
-					"maxclass" : "newobj",
-					"numinlets" : 0,
-					"numoutlets" : 0,
-					"patching_rect" : [ 230.0, 2.0, 77.0, 22.0 ],
-					"text" : "mira.channel"
-				}
-
-			}
-, 			{
-				"box" : 				{
-					"id" : "obj-2",
-					"maxclass" : "multislider",
-					"numinlets" : 1,
-					"numoutlets" : 2,
-					"orientation" : 0,
-					"outlettype" : [ "", "" ],
-					"parameter_enable" : 1,
-					"patching_rect" : [ 148.0, 106.0, 505.0, 242.0 ],
-					"saved_attribute_attributes" : 					{
-						"valueof" : 						{
-							"parameter_shortname" : "multislider",
-							"parameter_type" : 0,
-							"parameter_longname" : "multisliderX",
-							"parameter_mmax" : 500.0
-						}
-
-					}
-,
-					"setminmax" : [ 0.0, 500.0 ],
-					"varname" : "multislider"
-				}
-
-			}
-, 			{
-				"box" : 				{
 					"background" : 1,
-					"id" : "obj-5",
+					"id" : "obj-1",
 					"ignoreclick" : 1,
 					"maxclass" : "mira.frame",
 					"numinlets" : 0,
 					"numoutlets" : 0,
-					"patching_rect" : [ 69.0, 75.0, 853.80224609375, 607.000019068829261 ]
+					"patching_rect" : [ 195.0, 27.0, 256.000006437301636, 182.0 ]
 				}
 
 			}
  ],
 		"lines" : [ 			{
+				"patchline" : 				{
+					"destination" : [ "obj-6", 0 ],
+					"source" : [ "obj-11", 0 ]
+				}
+
+			}
+, 			{
 				"patchline" : 				{
 					"destination" : [ "obj-4", 0 ],
 					"source" : [ "obj-3", 0 ]
@@ -141,21 +194,29 @@
 			}
 , 			{
 				"patchline" : 				{
-					"destination" : [ "obj-6", 0 ],
-					"source" : [ "obj-4", 1 ]
+					"destination" : [ "obj-11", 0 ],
+					"source" : [ "obj-4", 3 ]
 				}
 
 			}
 , 			{
 				"patchline" : 				{
-					"destination" : [ "obj-7", 0 ],
+					"destination" : [ "obj-11", 1 ],
 					"source" : [ "obj-4", 2 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-10", 0 ],
+					"source" : [ "obj-6", 0 ]
 				}
 
 			}
  ],
 		"parameters" : 		{
-			"obj-2" : [ "multisliderX", "multislider", 0 ],
+			"obj-10" : [ "matrixctrl", "matrixctrl", 0 ],
+			"obj-5" : [ "slider", "slider", 0 ],
 			"parameterbanks" : 			{
 
 			}
